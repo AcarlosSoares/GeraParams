8aa89f6f1c38af9c1bd6c07639c1a024e0baeb0f
Anto Carlos Soares <acls.soares@gmail.com>
2020-01-09 18:12:06 -0200

    Atualização dia 09/01/2020 17:10

--- app/run.py
+++ app/run.py
@@ -18,44 +18,164 @@ objeto = {
 @app.route('/')
 def index():
 
-  return render_template("index.html", lista=objeto)
+  message = None
+
+  objeto_sorted = {k : objeto[k] for k in sorted(objeto.keys())}
+  return render_template("index.html", lista=objeto_sorted, error=message)
 
 
 @app.route('/incluir', methods=['POST'])
 def incluir():
 
-  val = 1
+  message = None
+
+  try:
+    gerarparams = request.form['gerar_params']
+    if gerarparams:
+      gerarParams()
+      message = "Arquivo 'geragrud.cnf' gerado com sucesso!"
+      objeto_sorted = {k : objeto[k] for k in sorted(objeto.keys())}
+      return render_template("index.html", lista=objeto_sorted, error=message)
+  except Exception as e:
+    gerarparams = None
+
+  conexao_bd = request.form['conexao_bd']
+  nome_bd = request.form['nome_bd']
+  nome_tabela = request.form['nome_tabela']
+  nome_model = request.form['nome_model']
+
+  if not conexao_bd:
+    message = "Conexão com BD deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  if not nome_bd:
+    message = "Nome do BD deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  if not nome_tabela:
+    message = "Nome da tabela do BD deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  if not nome_model:
+    message = "Nome da tabela do BD no Model deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  atributo = request.form['atributo']
+  if not atributo:
+    message = "Nome do atributo da tabela do BD deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  titulo = request.form['titulo']
+  if not titulo:
+    message = "Nome Titulo deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  tam_bd = request.form['tam_bd']
+  if not tam_bd:
+    message = "Tamanho do atributo no BD deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  tam_lista = request.form['tam_lista']
+  if not tam_lista:
+    message = "Tamanho do atributo na Lista de Consulta deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  tam_relat = request.form['tam_relat']
+  if not tam_relat:
+    message = "Tamanho do atributo na Lista do Relatório deve ser preenchido!"
+    return render_template("index.html", lista=objeto, error=message)
+    message = None
+
+  val = 0
   while val in objeto:
     val += 1
 
   new_list ={
-  'atributo':request.form['atributo'],
+  'atributo':atributo,
   'pk':request.form['pk'],
   'inc':request.form['inc'],
   'alt':request.form['alt'],
   'exc':request.form['exc'],
   'tipo':request.form['tipo'],
-  'titulo':request.form['titulo'],
-  'tam_bd':request.form['tam_bd'],
-  'tam_lista':request.form['tam_lista'],
-  'tam_relat':request.form['tam_relat']}
+  'titulo':titulo,
+  'tam_bd':tam_bd,
+  'tam_lista':tam_lista,
+  'tam_relat':tam_relat}
 
   objeto[val] = new_list
 
   objeto_sorted = {k : objeto[k] for k in sorted(objeto.keys())}
 
-  return render_template("index.html", lista=objeto_sorted)
+  return render_template("index.html", lista=objeto_sorted, error=message)
 
 
 @app.route('/excluir/<int:id>')
 def excluir(id):
 
+  message = None
+
   if id in objeto:
     del objeto[id]
 
   objeto_sorted = {k : objeto[k] for k in sorted(objeto.keys())}
+  return render_template("index.html", lista=objeto_sorted, error=message)
 
-  return render_template("index.html", lista=objeto_sorted)
+
+def gerarParams():
+
+  conexao_bd = request.form['conexao_bd']
+  nome_bd = request.form['nome_bd']
+  nome_tabela = request.form['nome_tabela']
+  nome_model = request.form['nome_model']
+
+  s = open("geracrud_novo.cnf","w", encoding='utf-8')
+
+  x = "list_form_key = '@@LIST_FORM@@'" + '\n'
+  x += "list_form_value = 'ListaForm'" + '\n'
+  s.write(x)
+
+  x = "insert_form_key = '@@INSERT_FORM@@'" + '\n'
+  x += "insert_form_value = 'IncluiForm'" + '\n'
+  s.write(x)
+
+  x = "update_form_key = '@@UPDATE_FORM@@'" + '\n'
+  x += "update_form_value = 'AlteraForm'" + '\n'
+  s.write(x)
+
+  x = "table_model_key = '@@TABLE_MODEL@@'" + '\n'
+  x += "table_model_value = '" + nome_model + "'" + '\n'
+  s.write(x)
+
+  x = "database_name_key = '@@DATABASE_NAME@@'" + '\n'
+  x += "database_name_value = '" + nome_bd + "'" + '\n'
+  s.write(x)
+
+  x = "table_name_key = '@@TABLE_NAME@@'" + '\n'
+  x += "table_name_value = '" + nome_tabela + "'" + '\n'
+  s.write(x)
+
+  x = "" + '\n'
+  s.write(x)
+
+  x = "insert_sql_key = '@@INSERT_SQL@@'" + '\n'
+  x += "update_sql_get_key = '@@UPDATE_SQL_GET@@'" + '\n'
+  x += "update_sql_post_key = '@@UPDATE_SQL_POST@@'" + '\n'
+  x += "print_list_key = '@@PRINT_LIST@@'" + '\n'
+  x += "ordenarpor_choices_key = '@@ORDENARPOR_CHOICES@@'" + '\n'
+  x += "models_attributes_key = '@@MODELS_ATTR@@'" + '\n'
+  s.write(x)
+
+  s.close()
+
+  print("GeraCrud", "Arquivo <geragrud.cnf> gerado com sucesso!")
 
 
 if __name__ == '__main__':
